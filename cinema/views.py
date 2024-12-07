7e6500ae4b0a9922789d58af080ae45af3b84d2d
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-12-07 13:45:37 +0200

    fix flake8

--- cinema/views.py
+++ cinema/views.py
@@ -5,8 +5,18 @@ from rest_framework import generics, mixins, viewsets
 
 from django.shortcuts import get_object_or_404
 
-from cinema.models import Movie, Genre, Actor, CinemaHall
-from cinema.serializers import MovieSerializer, GenreSerializer, ActorSerializer, CinemaHallSerializer
+from cinema.models import (
+    Movie,
+    Genre,
+    Actor,
+    CinemaHall
+)
+from cinema.serializers import (
+    MovieSerializer,
+    GenreSerializer,
+    ActorSerializer,
+    CinemaHallSerializer
+)
 
 
 class GenreList(APIView):
